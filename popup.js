dab5516975709268ae46e3dea3223a63099f2ebc
lb1a <luke.b3103@gmail.com>
2012-04-19 08:50:49 +0200

    fixing notifications

--- popup.js
+++ popup.js
@@ -7,21 +7,28 @@ function validateCredentials(){
     request.onreadystatechange = function(){
         if (request.readyState==4 &&  request.status==200){
             retrieve();
-        } else {
-        var notification = webkitNotifications.createNotification(
+        } else if (getShownotifications() && request.status != 200 ) {
+            var notification = webkitNotifications.createNotification(
                 'link_add.png',  // icon url - can be relative
                 'Authentication Error',  // notification title
                 'Your username or password is not correct.'  // notification body text
                 );
             notification.show();
         }
+        else {
+
+        }
     }
     request.send(params);
 }
 
 function retrieve() {
+    if(!validate()){
+        alert("Please set your credentials in the options of the extension!");
+        return;
+    }
     var apiurl = "https://readitlaterlist.com/v2/get";
-    var params = getPrefix()+"&apikey=eb7dmX46Taf23C2c57A2e87t0lg0Q529";
+    var params = getPrefix()+"&apikey=eb7dmX46Taf23C2c57A2e87t0lg0Q529&state=unread&";
     var request = new XMLHttpRequest();
     request.open("POST", apiurl, true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
