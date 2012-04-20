92f3f6cceea1aceea1ed1d4d1a806f4132391888
lb1a <luke.b3103@gmail.com>
2012-04-20 08:36:04 +0200

    added credentials check while saving to localStorage

--- popup.js
+++ popup.js
@@ -1,26 +1,3 @@
-function validateCredentials(){
-    var apiurl = "https://readitlaterlist.com/v2/auth";
-    var params = getPrefix()+"&apikey=eb7dmX46Taf23C2c57A2e87t0lg0Q529";
-    var request = new XMLHttpRequest();
-    request.open("POST", apiurl, true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.onreadystatechange = function(){
-        if (request.readyState==4 &&  request.status==200){
-            retrieve();
-        } else if (getShownotifications() && request.status != 200 ) {
-            var notification = webkitNotifications.createNotification(
-                'link_add.png',  // icon url - can be relative
-                'Authentication Error',  // notification title
-                'Your username or password is not correct.'  // notification body text
-                );
-            notification.show();
-        }
-        else {
-
-        }
-    }
-    request.send(params);
-}
 
 function retrieve() {
     if(!validate()){
@@ -39,4 +16,4 @@ function retrieve() {
     request.send(params);
 }
 
-validateCredentials();
+retrieve();
